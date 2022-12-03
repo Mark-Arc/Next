db3644f662e6591ea0be4696a4e9bdb2333654be
ajay <ajay@netaxis.io>
2022-12-03 12:12:13 +0530

    Next

--- next.config.js
+++ next.config.js
@@ -10,7 +10,21 @@ const withPWA = require('next-pwa')({
   runtimeCaching,
 });
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '/';
+let basePath = '';
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
 const nextConfig = withPWA({
+  assetPrefix,
+  basePath,
   reactStrictMode: true,
   swcMinify: true,
 });
